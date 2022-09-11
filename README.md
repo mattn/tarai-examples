a361e16becc13bf2afec755b5ff9d2c9a6e423ba
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-09-11 13:01:52 +0900

    update

--- README.md
+++ README.md
@@ -7,23 +7,23 @@ https://sumim.hatenablog.com/entry/2022/09/08/173557
 ```
 ==== C
 14
-real    0.623
+real    0.606
 system  0.000
-user    0.625
+user    0.593
 ==== Go
 14
-real    1.469
+real    1.378
 system  0.015
-user    1.453
+user    1.359
 ==== Zig
 14
-real    1.155
+real    1.000
 system  0.000
-user    1.156
+user    1.000
 ==== Rust
 14
-real    1.173
-system  0.015
-user    1.140
+real    1.056
+system  0.000
+user    1.046
 ```
 Windows 64bit Ryzen 5 3500U, 32GB RAM
--- tarai-rust.rs
+++ tarai-rust.rs
@@ -1,5 +1,3 @@
-use std::time::Instant;
-
 const fn tarai(x: i32, y: i32, z: i32) -> i32 {
     if x > y {
         tarai(tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y))
@@ -9,13 +7,5 @@ const fn tarai(x: i32, y: i32, z: i32) -> i32 {
 }
 
 fn main() {
-    let start_tarai = Instant::now();
-
     println!("{}", tarai(14, 7, 0));
-
-    let end_tarai = start_tarai.elapsed();
-
-    let tarai_sec = end_tarai.as_secs() as f64 + end_tarai.subsec_nanos() as f64 / 1000_000_000.0;
-
-    println!("{}", tarai_sec);
 }
